a1e8b91b18ed0ed01bd5c2c011e985144bc75122
chenshini <chensn456@163.com>
2014-05-30 19:47:43 +0800

    fix file share bugs

--- public/javascripts/file-share.js
+++ public/javascripts/file-share.js
@@ -1,5 +1,4 @@
-function fileMeta(hashCode,filename,holder,chunkCount,rank)
-{
+function fileMeta(hashCode, filename, holder, chunkCount, rank) {
     this.hashCode = hashCode;
     this.filename = filename
     this.holder = holder;
@@ -8,14 +7,14 @@ function fileMeta(hashCode,filename,holder,chunkCount,rank)
     this.queue = [];
     this.count = 0;
     this.rank = rank;
-} 
+}
 
 var fileShare = {};
 fileShare.events = {};
 
 fileShare.init = function (length) {
-    
-    
+
+
 
 }
 fileShare.chunkLength = 1000;
@@ -23,7 +22,7 @@ fileShare.on = function (eventname, fn) {
 
     fileShare.events[eventname] = fileShare.events[eventname] || [];
     fileShare.events[eventname].push(fn);
-}
+};
 fileShare.fire = function (eventname, _) {
 
     var events = this.events[eventname];
@@ -41,69 +40,31 @@ fileShare.send = function (receiver, space, data) {
         'space': space,
         'msg': data
     }));
-}
-fileShare.broadcast = function(receivers,space,data){
-	console.log(receivers);
-	receivers.forEach(function(r){
-		fileShare.send(r,space,data);
-	})
-}
-fileShare.dispatch = function(msg){
-	
-	fileShare.fire(msg.event,msg.data);
-}
+};
+fileShare.broadcast = function (receivers, space, data) {
+    console.log(receivers);
+    receivers.forEach(function (r) {
+        fileShare.send(r, space, data);
+    })
+};
+fileShare.dispatch = function (msg) {
 
-fileShare.createMeta = function(hashCode,filename,DataURL,holder,rank)
-{
-	var meta = new fileMeta(hashCode,filename,holder,Math.ceil(DataURL.length/fileShare.chunkLength),rank);
-	
-	meta.queue.push(holder);
-	while(DataURL.length>0){
-		meta.storage.push(DataURL.slice(0, fileShare.chunkLength));
-		DataURL = DataURL.slice(fileShare.chunkLength,DataURL.length);
-	}
-	fileShare.files[hashCode] = meta; 
-	return meta;
-	
-}
-/*fileShare.leave = function (callback) {
-    for (var i in fileShare.files) {
-        var meta = fileShare.files[i];
-        if (meta.holder == webrtc.socket) { //TODO: seperate webrtc
+    fileShare.fire(msg.event, msg.data);
+};
 
-        } else {
-            //inform sender that i want to leave
-            fileShare.send(meta.sender, 'fileShare', {
-                'event': 'receiver_leave',
-                'data': {
-                    'hashCode': meta.hashCode,
-                    'newReceiver': meta.receiver,
-                    'hasSentNo': meta.hasSentNo
-                }
+fileShare.createMeta = function (hashCode, filename, DataURL, holder, rank) {
+    var meta = new fileMeta(hashCode, filename, holder, Math.ceil(DataURL.length / fileShare.chunkLength), rank);
 
-            })
-            fileShare.send(meta.holder, 'fileShare', {
-                'event': 'someone_leave_when_sharing',
-                'data': {
-                    'someone': webrtc.socket,
-                    'hashCode': mata.hashCode,
-                }
-            });
-
-            if (!!meta.receiver) {
-                fileShare.send(meta.receiver, 'fileShare', {
-                    'event': 'sender_leave',
-                    'data': {
-                        'hashCode': meta.hashCode,
-                        'newSender': meta.sender
-                    }
-                })
-            }
-
-        }
+    meta.queue.push(holder);
+    while (DataURL.length > 0) {
+        meta.storage.push(DataURL.slice(0, fileShare.chunkLength));
+        DataURL = DataURL.slice(fileShare.chunkLength, DataURL.length);
     }
-    callback();
-}*/
+    fileShare.files[hashCode] = meta;
+    return meta;
+
+};
+
 fileShare.files = {};
 fileShare.on('someone_request_file', function (data) {
 
@@ -111,15 +72,15 @@ fileShare.on('someone_request_file', function (data) {
     var someone = data.someone;
     var hashCode = data.hashCode;
     var meta = fileShare.files[hashCode];
-    
-    
+
+
     var receivers = meta.queue.slice(1);
-    fileShare.broadcast(receivers,'fileShare',{
-	    'event':'new_sharer',
-	    'data':{
-		    'someone':someone,
-		    'hashCode':hashCode
-	    }
+    fileShare.broadcast(receivers, 'fileShare', {
+        'event': 'new_sharer',
+        'data': {
+            'someone': someone,
+            'hashCode': hashCode
+        }
     })
     meta.queue.push(someone);
     //inform receiver
@@ -131,11 +92,12 @@ fileShare.on('someone_request_file', function (data) {
             'holder': meta.holder,
             'queue': meta.queue,
             'chunkCount': meta.chunkCount,
-            'rank':meta.queue.length-2
+            'rank': meta.queue.length - 2
         }
     })
-    if(meta.queue.length == 2){
-	    holderSendChunk(hashCode,0);
+    if (meta.queue.length == 2) {
+        fileShare.holderSendChunk(hashCode, 0);
+       
     }
 
 });
@@ -148,11 +110,11 @@ fileShare.on('setup_for_sharing', function (data) {
     var rank = data.rank;
     var chunkCount = data.chunkCount;
     var queue = data.queue;
-    fileShare.files[hashCode] = new  fileMeta(hashCode, filename, holder,chunkCount,rank);
+    fileShare.files[hashCode] = new fileMeta(hashCode, filename, holder, chunkCount, rank);
     fileShare.files[hashCode].queue = queue;
 });
 
-function fileShareSaveToDisk(fileUrl, fileName) {
+fileShare.fileShareSaveToDisk = function (fileUrl, fileName) {
     console.log(fileName + 'save to disk');
     var save = document.createElement('a');
     save.href = fileUrl;
@@ -173,23 +135,23 @@ fileShare.on('receive_chunk', function (data) {
     var meta = fileShare.files[hashCode];
     var index = data.index;
     var sender = data.sender;
-    if(!meta.storage[index]){
-	    meta.storage[index] = chunk;
-	    meta.count++;
+    if (!meta.storage[index]) {
+        meta.storage[index] = chunk;
+        meta.count++;
     }
-    console.log('receive chunk (file share) ' + meta.storage);
+    console.log('receive chunk (file share) index = ' + index);
     if (meta.count == meta.chunkCount) {
-    
-        fileShareSaveToDisk(meta.storage.join(''), meta.filename);
+
+        fileShare.fileShareSaveToDisk(meta.storage.join(''), meta.filename);
         //inform file provider that someone has received file succesfully
         /*fileShare.send(meta.holder, 'htmlRender', {
-            'event': 'systemMessage',
-            'data': {
-                'msg': '<strong>' + _nickname + '</strong>已经成功接收' + meta.filename + '.' //TODO: get nick name from meta.
-            }
+        'event': 'systemMessage',
+        'data': {
+        'msg': '<strong>' + _nickname + '</strong>已经成功接收' + meta.filename + '.' //TODO: get nick name from meta.
+        }
         });*/
     }
-    if (data.sender!= meta.holder)
+    if (data.sender != meta.holder)
         return;
     var receivers = meta.queue.slice(1);
     fileShare.broadcast(receivers, 'fileShare', {
@@ -197,23 +159,23 @@ fileShare.on('receive_chunk', function (data) {
         'data': {
             'chunk': chunk,
             'hashCode': hashCode,
-            'index':index,
-            'sender':webrtc.socket    //TODO: remove webrtc
+            'index': index,
+            'sender': webrtc.socket    //TODO: remove webrtc
         }
     });
-   
+
 
 });
 
 fileShare.on('new_sharer', function (data) {
-	var someone = data.someone;
+    var someone = data.someone;
     var queue = data.queue;
     var hashCode = data.hashCode;
-    
+
     var meta = fileShare.files[hashCode];
     //console.log('debug (new_receiver) ' + JSON.stringify(meta));
     meta.queue.push(someone);
-    fileShareSendChunk(meta.rank, meta.storage.length, meta,hashCode,meta.queue.length-2);
+    fileShare.fileShareSendChunk(someone,meta.rank, meta.storage.length, meta, hashCode, meta.queue.length - 2);
     //for (var i = meta.hasSentNo + 1, end = meta.storage.length; i < end; i++) {
 
     //    var data = {};
@@ -229,52 +191,50 @@ fileShare.on('new_sharer', function (data) {
 
     //}
 })
-function fileShareSendChunk(i,end,meta,hashCode,cnt)
-{
-    console.log('debug in file share send chunk i='+i+' end='+end);
-    if(i<end)
-    {
+fileShare.fileShareSendChunk = function (someone, i, end, meta, hashCode, cnt) {
+    console.log('debug in file share send chunk i=' + i + ' end=' + end);
+    if (i < end) {
         var data = {};
         data.chunk = meta.storage[i];
         data.hashCode = hashCode;
         data.sender = webrtc.socket; //TODO: seperate webrtc 
         data.index = i;
-        fileShare.send(meta.receiver, 'fileShare', {
+        fileShare.send(someone, 'fileShare', {
             'event': 'receive_chunk',
             'data': data
         });
-        i+=cnt;
+        i += cnt;
         setTimeout(function () {
-            fileShareSendChunk(i, end, meta,hashCode,cnt);
+            fileShare.fileShareSendChunk(someone, i, end, meta, hashCode, cnt);
         }, 500);
     }
-    
+
 }
 
-function holderSendChunk(hashCode,start){
-	console.log('debug (holderSendChunk '+ start);
-	var meta = fileShare.files[hashCode];
-	var cnt = meta.queue.length;
-	var end = meta.chunkCount;
-	var receivers = meta.queue.slice(1);
-	var index = start;
-	for(var i in receivers){
-		if(start>=end) break;
-		fileShare.send(receivers[i],'fileShare',{
-			'event':'receive_chunk',
-			'data':{
-				'chunk': meta.storage[start],
-				'hashCode': meta.hashCode,
-				'index':index,
-				'sender':webrtc.socket
-			}
-		})
-		index ++;
-	}
-	if(index<end)
-		setTimeout(function(){
-			holderSendChunk(meta.hashCode,index);
-		}, 500)
+fileShare.holderSendChunk = function (hashCode, start) {
+    console.log('debug (holderSendChunk ' + start);
+    var meta = fileShare.files[hashCode];
+    var cnt = meta.queue.length;
+    var end = meta.chunkCount;
+    //var receivers = meta.queue.slice(1);
+    var index = start;
+    for (var i = 1; i < cnt; i++) {
+        if (start >= end) return;
+        fileShare.send(meta.queue[i], 'fileShare', {
+            'event': 'receive_chunk',
+            'data': {
+                'chunk': meta.storage[index],
+                'hashCode': meta.hashCode,
+                'index': index,
+                'sender': webrtc.socket
+            }
+        });
+        index++;
+    }
+    if (index < end)
+        setTimeout(function () {
+            fileShare.holderSendChunk(meta.hashCode, index);
+        }, 500);
 }
 
 /*function fileMeta(hashCode,filename,holder,sender,receiver,chunkCount)
